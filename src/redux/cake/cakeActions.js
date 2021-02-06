47f1bfb7333bfab4c5c38e9d8c9ce20418e352db
Dulan Wirajith <dulanwirajith1995@gmail.com>
2021-02-06 20:24:57 +0530

    cakeAction changes

--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -1,20 +1,5 @@
 import { BUY_CAKES } from "./cakeTypes";
 
-export const getPosts = () => async (dispatch) => {
-  try {
-    const { data } = await api.fetchPosts();
-    // api
-    //   .fetchPosts()
-    //   .then((response) => {
-    //     console.log("response is" + response);
-    //     dispatch({ type: "FETCH_ALL", payload: response });
-    //   })
-    //   .catch((error) => {
-    //     console.log(error);
-    //   });
-    console.log("response is" + data);
-    dispatch({ type: BUY_CAKES, payload: data });
-  } catch (error) {
-    console.log(error.message);
-  }
+export const buyCakes = () => async (dispatch) => {
+  return { type: BUY_CAKES, payload: data };
 };
